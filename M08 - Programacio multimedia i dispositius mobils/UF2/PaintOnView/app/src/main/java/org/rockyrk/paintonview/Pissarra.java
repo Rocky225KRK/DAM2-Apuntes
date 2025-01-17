8dcc81d0de501f4c8b044d62a428f9b77eb9a604
Rocky225KRK <polix225@gmail.com>
2025-01-17 16:52:30 +0100

    Update PaintOnView

--- M08 - Programacio multimedia i dispositius mobils/UF2/PaintOnView/app/src/main/java/org/rockyrk/paintonview/Pissarra.java
+++ M08 - Programacio multimedia i dispositius mobils/UF2/PaintOnView/app/src/main/java/org/rockyrk/paintonview/Pissarra.java
@@ -1,23 +1,31 @@
 package org.rockyrk.paintonview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class Pissarra extends View {
+    private Bitmap ghost;
+    private int frame=0;
+
     public Pissarra(Context context) {
-        super(context);
+        this(context,null);
     }
 
     public Pissarra(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        ghost =BitmapFactory.decodeResource(this.getResources(),R.drawable.red);
     }
 
     @Override
@@ -39,27 +47,43 @@ public class Pissarra extends View {
         float r=200;
         float xCentre=300,yCentre=300;
         float xA=-1,yA=-1;
-        int lap=10;
+        int lap=5;
 
-        for(float angle=0;angle<=360*lap;angle+=10){
-            float x=(float) (r*Math.cos(Math.toRadians(angle)))+xCentre;
-            float y=(float) (r*Math.sin(Math.toRadians(angle)))+yCentre;
+        for(float angle=0;angle<=360*lap;angle+=10) {
+            float x = (float) (r * Math.cos(Math.toRadians(angle))) + xCentre;
+            float y = (float) (r * Math.sin(Math.toRadians(angle))) + yCentre;
 
-            if(xA!=-1){
+            if (xA != -1) {
                 //canvas.drawLine(xA,yA,x,y,paint);
-                p.lineTo(x,y);
-            }else{
-                p.moveTo(x,y);
+                p.lineTo(x, y);
+            } else {
+                p.moveTo(x, y);
             }
 
             // Espiral
             //r-=1;
 
             // Golden ratio (espiral millorada)
-            r-= 200.0f /((360.0f*lap)/10.0f);
-            xA=x;
-            yA=y;
+            r -= 200.0f / ((360.0f * lap) / 10.0f);
+            xA = x;
+            yA = y;
         }
+        p.quadTo(0,2200,800,2200);
+
         canvas.drawPath(p,paint);
+
+        //canvas.drawBitmap(ghost,100,100,paint);
+        int ghostX;
+        ghostX=frame*ghost.getWidth()/2;
+        canvas.drawBitmap(ghost,new Rect(ghostX,0, ghost.getWidth()/2, ghost.getHeight()),
+                new Rect(0,0,ghost.getWidth()/2,ghost.getHeight()),
+                paint);
+        frame=(frame+1)%2;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        invalidate();
+        return true;
     }
 }
